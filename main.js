44e03675b1ebf8cd04e957e878b36d5a02e8c9f8
Abir69609 <abirfuego69@gmail.com>
2021-06-20 17:29:37 +0600

    my third site

---
+++ main.js
@@ -0,0 +1,10 @@
+          // Created By Abir
+          
+
+alert("\n       Tap anywhere to enable music\n");
+  
+window.addEventListener("mouseover",play);
+window.addEventListener("click",play);
+function play(){ document.getElementById("audio").play();
+}
+  
